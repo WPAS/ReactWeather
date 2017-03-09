0a2d30132f2903ed60145e87bdf85e81c7755f15
Wojciech Pastuszka <Wojciech Pastuszka>
2017-03-09 14:23:25 +0100

    add Nav styles

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,14 +1,38 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () =>  {
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert("Not yet wired up");
+  },
+
+  render: function() {
     return (
-      <div>
-        <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active">About</Link>
-        <Link to="/examples" activeClassName="active">Examples</Link>
-      </div>
+      <nav className='top-bar'>
+        <div className='top-bar-left'>
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li><IndexLink to="/" activeClassName="active">Get Weather</IndexLink></li>
+            <li><Link to="/about" activeClassName="active">About</Link></li>
+            <li><Link to="/examples" activeClassName="active">Examples</Link></li>
+          </ul>
+        </div>
+        <div className='top-bar-right'>
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather" />
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather" />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </nav>
     );
-};
+  }
+});
 
 module.exports = Nav;
