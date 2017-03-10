ac51513611c83e914d92c6f599d849c98edc954b
Wojciech Pastuszka <Wojciech Pastuszka>
2017-03-10 08:31:15 +0100

    add styles to About and Weathers components

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
     return(
       <div>
-        <h3>About</h3>
-        <p>This is simple React application from 'The Complete React Web App Developer Course' by Andrew Mead</p>
+        <h1 className='text-center'>About</h1>
+        <p>This is a simple React application from
+           <em>'The Complete React Web App Developer Course'</em> by Andrew Mead.
+        </p>
+        <p>Here are some of the tools used in the app:</p>
+        <ul>
+          <li><a href="https://facebook.github.io/react">React.js</a></li>
+          <li><a href="https://github.com/ReactTraining/react-router">React Router</a></li>
+          <li><a href="http://openweathermap.org">Open Weather Map</a></li>
+          <li><a href="http://foundation.zurb.com/">Foundation</a></li>
+        </ul>
       </div>
     )
 };
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var WeatherMessage = ({temp, location}) => {
     return (
       <div>
-        <p>There is {temp}<sup>o</sup>C in {location}</p>
+        <h4 className="text-center">There is {temp}<sup>o</sup>C in {location}</h4>
       </div>
     )
 };
